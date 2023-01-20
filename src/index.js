682377a0b045b7c8c81a68ba0aa38e505bdd19df
Andrew Waddell <20awaddell@gmail.com>
2023-01-20 15:34:25 -0500

    React-Tutorial

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+//ReactDOM.render(element, document.getElementById('root'));
+
+reportWebVitals();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+root.render(element)(
   <React.StrictMode>
     <App />
+    element
   </React.StrictMode>
 );
 
